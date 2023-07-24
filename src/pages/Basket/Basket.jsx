20aae28ec6275014abe67f6941fcb76a7fc0b8f1
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-07-24 15:45:49 +0300

    code refactoring and remove func 'isready'

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,38 +1,23 @@
 import {useDispatch,useSelector} from 'react-redux'
-import { useEffect} from 'react';
-import clsx from 'clsx';
 
 import Button from 'react-bootstrap/esm/Button';
 
 import style from './Basket.module.scss'
 
-const Basket = (props) => {
-
+const Basket = () => {
     const dispatch = useDispatch();
     const curProducts = useSelector(state=>state.items.basket)
     const REMOVE_FROM_BASKET = 'REMOVE_FROM_BASKET'
-    function isReady (curPr){
-        return curPr
-    }
-    useEffect(()=>{
-        isReady(curProducts)    
 
-    },[curProducts]);
-    const showBaskety = props.showsBasket;
-    
     function deleteIt(id) {
         dispatch({type:REMOVE_FROM_BASKET,payload:id}) 
         
     }
-    const showBasketClass = clsx({
-        [style.basket]:true,
-        [style.active]:!showBaskety,
-        [style.notactive]:showBaskety
-    })
     return (
         <div>
             Корзина
-            <div className={showBasketClass} >
+            <div className={style.box}>
+            <div className={style.basket} >
                 <div className={style.basket__list}>
                     {!curProducts.length ? 'Корзина пуста' : curProducts.map((it,i) => (
                         
@@ -50,7 +35,9 @@ const Basket = (props) => {
                     )
                     )}
                 </div>
+            </div>    
             </div>
+            
         </div>
     );
 };
