551f284df51c5d6519202ed1d519cdca61de962d
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-07-28 11:03:29 +0300

    fix bugs and conflcts

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { NavLink, Route, Routes } from "react-router-dom";
 
-import MainPage from "./pages/MainPage/MainPage";
-import Basket from "./pages/Basket";
-import NotFound from "./pages/Not found/Not found";
-import { ROOT_TO_MAINPAGE,ROOT_TO_BASKET } from './Routes/routes'
+import MainPage from './pages/MainPage';
 
 import './App.css';
-
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       <Container>
-      <div>
-      <nav className='navigation'>
-        <li>
-          <NavLink
-            to={ROOT_TO_MAINPAGE}
-            className='links'
-            style={({ isActive }) => {
-              return isActive ? { background: "#FFB4EA" } : {};
-            }}
-          >
-            Back
-          </NavLink>
-          <NavLink
-            className='links'
-            to={ROOT_TO_BASKET}
-            style={({ isActive }) => {
-              return isActive ? { background: "#FFB4EA" } : {};
-            }}
-          >
-            Basket
-          </NavLink>
-        </li>
-      </nav>
-      <div className='box'>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/mainpage" element={<MainPage />} />
-          <Route path="/basket" element={<Basket />} />
-        </Routes>
-      </div>
-    </div>
+        <MainPage />
       </Container>
     </div>
   );
 }
-
 export default App;
