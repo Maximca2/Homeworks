b075d7166731b955be092a21eb5905195e36a751
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-07-31 19:37:21 +0300

    sort imports and refactoring

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,11 @@
-import {createStore ,combineReducers,applyMiddleware} from 'redux'
-import { itemsReducer } from './ItemsReduce';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+
+import { itemsReducer } from './itemsReduce';
+
 const rootReducer = combineReducers({
-    items:itemsReducer,
+    items: itemsReducer,
 })
 
-export const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
