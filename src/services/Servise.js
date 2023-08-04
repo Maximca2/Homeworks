de35b06cf77dfbcf6e83f2f660e5c751286f47f8
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-04 12:07:17 +0300

    i change default fetch to axios

--- src/services/Servise.js
+++ src/services/Servise.js
@@ -1,10 +1,20 @@
+import axios from 'axios';
+
+import { userApi } from "../api/api"
+
 import { addCustomersAction } from "../redux/store/ItemsReduce"
 
 export const fetchUsers = () => {
+
   return function (dispatch) {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => dispatch(addCustomersAction(json)))
+  axios.get(userApi)
+
+  .then(({data})=>dispatch(addCustomersAction(data)))
+
+  .catch(function (error) {
+    console.log(error);
+  })
   }
+
 }
 
