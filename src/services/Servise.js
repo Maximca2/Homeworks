2dc7011aeae307f40549aafea21a580e1f914c97
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-07 11:32:47 +0300

    implement function fetchForId

--- src/services/Servise.js
+++ src/services/Servise.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 import { fetchUser } from '../redux/store/usersReducer'
+import { fetchUserId } from '../redux/store/usersReducer';
 
 export const fetchUsers = () => {
 
@@ -16,4 +17,18 @@ export const fetchUsers = () => {
   }
 
 }
+export const fetchforId = (id) => {
+    
+  return function (dispatch) {
 
+    axios.get(`${process.env.REACT_APP_API}/users/${id.userId}`)
+      .then(({ data }) =>
+        dispatch(fetchUserId(data),console.log(data)))
+
+      .catch(function (error) {
+        console.error(error)
+      })
+  }
+  
+
+}
