3191143f8716ae3e62b150011f30fcc18dcf56b2
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-07 13:30:33 +0300

    some changes which i return

--- src/redux/store/usersReducer.js
+++ src/redux/store/usersReducer.js
@@ -1,6 +1,6 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
 
-import { ADD_TO,REMOVE_FROM,FETCH_USERS,FETCH_USERS_ID } from './actions';
+import { ADD_TO,REMOVE_FROM_BASKET,FETCH_USERS,FETCH_USERS_ID } from './actions';
 
 import imf from '../../img/bag_rose.jpg'
 
@@ -47,7 +47,7 @@ const defaultState = {
 }
 // actions
 export const addUser = createAction(ADD_TO);
-export const removeUser =createAction(REMOVE_FROM);
+export const removeUser =createAction(REMOVE_FROM_BASKET);
 export const fetchUser = createAction(FETCH_USERS)
 export const fetchUserId = createAction(FETCH_USERS_ID)
 
