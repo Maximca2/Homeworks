2f0f42abc0111587ca39e44595ed0cd4a11742e8
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-12 14:55:50 +0300

    add LeafletMap -''Map'

---
+++ src/pages/ReactLeafletMap/LeafletMap.jsx
@@ -0,0 +1,27 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "../ReactLeafletMap/leaflet.scss";
+import "leaflet/dist/leaflet.css";
+
+export default function LeafletMap({ cordinates }) {
+  if (!cordinates) {
+    return;
+  } else if (cordinates.lat && cordinates.lng) {
+    return (
+      <>
+        <MapContainer center={cordinates} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={cordinates}>
+            <Popup>
+              im live here <br />.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </>
+    );
+  }
+  
+}
