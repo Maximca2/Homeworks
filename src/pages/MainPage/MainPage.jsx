cd63219c91d3fe11c8eed5f842c9806b18818a1b
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-15 12:46:53 +0300

    aad filterData and input to show our users

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 import { NavLink } from "react-router-dom";
 
 import { fetchUsers } from "../../services/servise";
-import { addUser } from "../../redux/store/usersReducer";
+import { addUser} from "../../redux/store/usersReducer";
 import { ROUTE_TO_ABOUT_USER } from "../../routes/routes";
 
 import SkeletonForMainpageCards from "../../components/Skeletons/SkeletonMainPageCards/SkeletonForMainpageCards";
@@ -21,19 +21,29 @@ const MainPage = () => {
   const users = useSelector((state) => state.toolkit.users)[0];
   const [showSkeleton, setShowSkeleton] = useState(true);
   const [show, setShow] = useState(true);
-
+  const [inputValue, setInputValue] = useState("");
+  const [additionStatetoUsers, setadditionStatetoUsers] = useState(users);
   const curProducts = useSelector((state) => state.toolkit.basket);
-
+  useEffect(()=>{
+    if(!inputValue){
+      setadditionStatetoUsers(users)
+    }
+  },[inputValue])
+  useEffect(()=>{
+    if(users){
+      setadditionStatetoUsers(users)
+    }
+  },[users])
   useEffect(() => {
     const timer = setTimeout(() => {
       dispatch(fetchUsers());
+      
     }, 2000);
     return () => {
       clearTimeout(timer);
       setShowSkeleton(false);
     };
   }, [dispatch]);
-
   const addToCart = (product, i) => {
     const currentProduct = {
       product,
@@ -45,6 +55,26 @@ const MainPage = () => {
   function showBasket(cond) {
     setShow(cond);
   }
+  const FilterData = (event) => {
+    const str = event.target.value;
+    const ourArrforfilterUser = [];
+    setInputValue(str);
+
+    if (users) {
+      for (let index = 0; index < users.length; index++) {
+        const userObj = users[index];
+        const address = Object.values(userObj.address);
+        const company = Object.values(userObj.company);
+        const allValue = Object.values(userObj).concat(company).concat(address);
+
+        if (allValue.includes(str)) {
+          ourArrforfilterUser.push(userObj);
+        }
+        setadditionStatetoUsers(ourArrforfilterUser);
+      }
+      
+    }
+  };
   return (
     <div>
       <Container>
@@ -61,20 +91,26 @@ const MainPage = () => {
           </header>
           <main>
             <div className={style.box__list}>
-              {!users || showSkeleton ? (
+              <div className={style.input__box}>
+                <label>
+                  Input Value:
+                  <input type="text" value={inputValue} onChange={FilterData} />
+                </label>
+                <p>Input Value: {inputValue}</p>
+              </div> 
+              {!additionStatetoUsers || showSkeleton ? (
                 <>
                   <SkeletonForMainpageCards cardsLength={4} />
                 </>
-              ) : (
-                users.map((it, i) => {
-                  const { name, company, id,} = it;
+              ) : (additionStatetoUsers.map((it, i) => {
+                  const { name, company, id } = it;
                   return (
-                    <div key={i} className={style.box__card}>
+                    <div key={i}  className={style.box__card}>
                       <div className={style.box__img}>
                         <img src={imgPerson} alt="img_bags" />
                       </div>
-                      <div className="">{name}</div>
-                      <div className=""> Company:{company.name}</div>
+                      <div >{name}</div>
+                      <div > Company:{company.name}</div>
 
                       <Button
                         className={style.button41}
