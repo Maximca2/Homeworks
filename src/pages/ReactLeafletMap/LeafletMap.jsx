efdfb58ad257eb16fd0631e834067163884402ed
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-15 08:04:20 +0300

    add content to popup

--- src/pages/ReactLeafletMap/LeafletMap.jsx
+++ src/pages/ReactLeafletMap/LeafletMap.jsx
@@ -3,19 +3,23 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "../ReactLeafletMap/leaflet.scss";
 import "leaflet/dist/leaflet.css";
 
-export default function LeafletMap({ cordinates }) {
-
+export default function LeafletMap({ cordinates ,name}) {
+  
   if (cordinates.lat) {
+  
     return (
       
-        <MapContainer center={cordinates} zoom={13}>
+        <MapContainer center={cordinates}  zoom={15}>
+          
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <Marker position={cordinates}>
             <Popup>
-              im live here <br />.
+              im live here <br /> 
+              Im {name}
+              my cord  lat{cordinates.lat}{cordinates.lng}
             </Popup>
           </Marker>
         </MapContainer>
@@ -23,6 +27,4 @@ export default function LeafletMap({ cordinates }) {
     );
   }
     return
-
-   
 }
