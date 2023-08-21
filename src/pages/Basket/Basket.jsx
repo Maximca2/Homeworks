35201b4d25c9eeb31035eb93d965bbe6288dd2a1
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-08-21 12:44:22 +0300

    some ref

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -12,36 +12,36 @@ const Basket = ({img}) => {
   function deleteIt(id) {
     dispatch(removeUser(id));
   }
-  // return (
-  //   <div>
-  //     List of adding workers
-  //     <div className={style.box}>
-  //       <div className={style.basket}>
-  //         <div className={style.basket__list}>
-  //           {!curProducts.length
-  //             ? "Корзина пуста"
-  //             : curProducts.map(({ product }, i) => {
-  //                 const { name, price,id} = product;
-  //                 return (
-  //                   <div key={i} className={style.basket__card}>
-  //                     <img src={img} alt="curimg" />
-  //                     {name}
-  //                     {price}
-  //                     <Button className={style.button41}>Купити</Button>
-  //                     <Button
-  //                       onClick={() => deleteIt(id)}
-  //                       className={style.button42}
-  //                     >
-  //                       Видалити
-  //                     </Button>
-  //                   </div>
-  //                 );
-  //               })}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div>
+      List of adding workers
+      <div className={style.box}>
+        <div className={style.basket}>
+          <div className={style.basket__list}>
+            {!curProducts.length
+              ? "Корзина пуста"
+              : curProducts.map(({ product }, i) => {
+                  const { name, price,id} = product;
+                  return (
+                    <div key={i} className={style.basket__card}>
+                      <img src={img} alt="curimg" />
+                      {name}
+                      {price}
+                      <Button className={style.button41}>Купити</Button>
+                      <Button
+                        onClick={() => deleteIt(id)}
+                        className={style.button42}
+                      >
+                        Видалити
+                      </Button>
+                    </div>
+                  );
+                })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Basket;
